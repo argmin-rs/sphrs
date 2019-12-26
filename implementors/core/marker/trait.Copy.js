62ab5942b0f47b33f815ea54dfe1f2838228bac3
Stefan Kroboth <stefan.kroboth@gmail.com>
2019-12-26 10:05:10 +0100

    Semiautomatic update of documentation

--- implementors/core/marker/trait.Copy.js
+++ implementors/core/marker/trait.Copy.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["sphrs"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"sphrs/enum.RealSHType.html\" title=\"enum sphrs::RealSHType\">RealSHType</a>",synthetic:false,types:["sphrs::RealSHType"]},];
+implementors["sphrs"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"sphrs/enum.RealSHType.html\" title=\"enum sphrs::RealSHType\">RealSHType</a>",synthetic:false,types:["sphrs::RealSHType"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"sphrs/enum.ComplexSHType.html\" title=\"enum sphrs::ComplexSHType\">ComplexSHType</a>",synthetic:false,types:["sphrs::ComplexSHType"]},];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
